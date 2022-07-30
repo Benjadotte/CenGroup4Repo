7f15b4abd7b7f0e120078987e87d7ed077a9f417
Benjadotte <dulcio256@yahoo.com>
2022-07-29 21:31:08 -0400

    Latest

--- src/main/java/net/javaguides/bookstore/BookstoreApplication.java
+++ src/main/java/net/javaguides/bookstore/BookstoreApplication.java
@@ -2,13 +2,15 @@ package net.javaguides.bookstore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication(exclude = {UserDetailsServiceAutoConfiguration.class})
 @Configuration
-@ComponentScan({"net.javaguides.bookstore"})
+@ComponentScan(basePackages = "net.javaguides.*")
+@EntityScan("net.javaguides.*")
 
 	public class BookstoreApplication {
 
